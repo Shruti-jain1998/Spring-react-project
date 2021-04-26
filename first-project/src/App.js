bf3bfab189648f1a70bd11415b3b47ab0b83f5a4
Shruti-jain1998 <shruti02081998@gmail.com>
2021-04-26 02:48:00 -0700

    initial-commit

---
+++ first-project/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+
+import './App.css';
+import UserComponent from './components/UserComponent';
+
+
+function App() {
+  return (
+    <div className="App">
+     <UserComponent/>
+
+    </div>
+  );
+}
+
+export default App;
